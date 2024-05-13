57047bd5674fec4e1c44dbb2aa152805b3b8c324
Carla Lopez <calopez@student.ethz.ch>
2024-05-13 16:03:48 +0200

    plots

--- plotting/plot_initial_distribution.py
+++ plotting/plot_initial_distribution.py
@@ -5,10 +5,7 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 # Change this path to the path of the txt file
-PAR_DIST="EquidistantDistribution"
-VOR_DIST="Disk"
-
-PATH=f"{PAR_DIST}_{VOR_DIST}"
+PATH="../build_serial/alvine"
 
 # Load data from CSV file
 df = pd.read_csv(f'{PATH}/particles.csv')
@@ -16,13 +13,13 @@ df = pd.read_csv(f'{PATH}/particles.csv')
 initial_cong = df[df['time']==1]
 
 fig, ax = plt.subplots(figsize=(10, 10))
-ax.set_xlim((0,30))
-ax.set_ylim((0,30))
+ax.set_xlim((0,10))
+ax.set_ylim((0,10))
 
-scat = ax.scatter([], [], c=[], s=1, cmap='viridis_r')
+scat = ax.scatter([], [], c=[], s=1, cmap='viridis')
 scat.set_offsets(np.c_[initial_cong['pos_x'], initial_cong['pos_y']])
 scat.set_array(initial_cong['vorticity'])  # Set colors based on vorticity
 
 # Show animation
 # plt.show()
-plt.savefig(f'{PATH}/init_config.png')
+plt.savefig(f'init_config.png')
